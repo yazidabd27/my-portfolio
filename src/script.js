41c3801d878c149b8f0bd7610eb18dece284dfd9
yazid <yazidabd35@gmail.com>
2023-08-18 11:06:36 +0100

    first commit

---
+++ src/script.js
@@ -0,0 +1,47 @@
+
+const menuIcon=document.getElementById('menu-icon');
+const closeIcon=document.getElementById('close-icon');
+const navBar=document.querySelector('nav');
+
+menuIcon.onclick=()=>{
+    navBar.classList.remove('-translate-x-full');
+    menuIcon.classList.add('hidden');
+    closeIcon.classList.remove('hidden');
+}
+
+closeIcon.onclick=()=>{
+    navBar.classList.add('-translate-x-full');
+    menuIcon.classList.remove('hidden');
+    closeIcon.classList.add('hidden');
+}
+
+const navLinks=document.querySelectorAll('nav a');
+const sections=document.querySelectorAll('section');
+
+navLinks.forEach(link=>{
+    link.onclick=()=>{
+        disable();
+        link.classList.add('active');
+    }
+})
+
+window.onscroll=()=>{
+    let top=window.scrollY;
+    
+    sections.forEach(section=>{
+        let offset=section.offsetTop;
+        let height=section.offsetHeight;
+        let id=section.getAttribute('id');
+
+        if(top>=offset && top<offset+height){
+            disable();
+            document.querySelector(`a[href='#${id}']`).classList.add('active');
+        }
+    })
+
+function disable(){
+    navLinks.forEach(link=>{
+        link.classList.remove('active');
+    })
+}
+}
---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    extend: {
+      colors:{
+        bl: '#1E2A3A',
+        cy:'#00ADB5',
+        gr:'#e5e7eb'
+      }
+    },
+  },
+  plugins: [],
+}
+
